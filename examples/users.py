bc48dd6e6b28406874cb3c3fda6c91cc90c77bb7
vtemian <vladtemian@gmail.com>
2014-05-16 12:59:43 +0300

    Introduce a complete user operations example

--- examples/users.py
+++ examples/users.py
@@ -4,5 +4,14 @@ from pyolite import Pyolite
 admin_repository = 'gitolite-admin/'
 olite = Pyolite(admin_repository=admin_repository)
 
-bob = olite.users.create(name='bob', key='my-awesome-key')
-alice = olite.users.create(name='alice', key_path='~/.ssh/id_rsa.pub')
+# create user object
+vlad = olite.users.create(name='vlad', key_path='/home/wok/.ssh/id_rsa.pub')
+
+# get user by name
+vlad = olite.users.get(name='vlad')
+
+# get_or_create django style
+vlad = olite.users.get_or_create(name='vlad')
+
+# check if user is admin or not
+print vlad.is_admin
