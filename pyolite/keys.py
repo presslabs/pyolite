fb3df0f5487989f6d3bdc75f1ca182eff0912af4
vtemian <vladtemian@gmail.com>
2014-05-16 17:23:14 +0300

    Special key list

---
+++ pyolite/keys.py
@@ -0,0 +1,31 @@
+import hashlib
+
+from unipath import Path
+
+
+class ListKeys(list):
+  def __init__(self, user, *args, **kwargs):
+    super(ListKeys, self).__init__(*args, **kwargs)
+    self.user = user
+
+  def append(self, key):
+    key = Path(self.user.path, key)
+
+    if key.isfile():
+      with open(key) as f:
+        key = f.read()
+
+    directory = Path(self.user.path, 'keydir', hashlib.md5(key).hexdigest())
+    directory.mkdir()
+
+    key_file = Path(directory, "%s.pub" % self.user.name)
+    key_file.write_file(key)
+
+    self.user.git.commit(['keydir'],
+                         'Added new key for user %s' % self.user.name)
+
+    super(ListKeys, self).append(key)
+
+  def __add__(self, keys):
+    for key in keys:
+      self.append(key)
