5cae36b8eb155fb2de6ddb4905080b6127045930
vtemian <vladtemian@gmail.com>
2014-05-16 12:28:48 +0300

    Implemented get method for UserManager

--- pyolite/managers/user.py
+++ pyolite/managers/user.py
@@ -7,9 +7,6 @@ from .manager import Manager
 
 
 class UserManager(Manager):
-  def get(self, user):
-    pass
-
   def create(self, name, key=None, key_path=None):
     if key is None and key_path is None:
       raise ValueError('You need to specify a key or key_path')
@@ -25,4 +22,7 @@ class UserManager(Manager):
     key_file.write_file(key)
 
     self.git.commit(['keydir'], 'Added new key for user %s' % name)
+    return User.get_by_name(name, self.path)
+
+  def get(self, name):
     return User.get_by_name(name, self.path)
