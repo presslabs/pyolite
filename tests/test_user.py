315722e1237250cf0756962cfc5c7a80eb41c102
vtemian <vladtemian@gmail.com>
2014-05-19 14:51:00 +0300

    Test: if a key and a username is supplied, it should return a new user

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from mock import MagicMock, patch
-from nose.tools import raises
+from nose.tools import raises, eq_
 
 from pyolite.managers.user import UserManager
 
@@ -14,3 +14,18 @@ class TestUser(TestCase):
                         Path=MagicMock()):
       users = UserManager('~/path/to/admin/gitolite/repo')
       users.create('test_username')
+
+  def test_create_user_succesfully(self):
+    mocked_user = MagicMock(return_value='test_username')
+    mocked_git = MagicMock()
+    mocked_path = MagicMock()
+
+    with patch.multiple('pyolite.managers.manager',
+                        Git=MagicMock(return_value=mocked_git),
+                        Path=MagicMock(return_value=mocked_path)):
+      with patch.multiple('pyolite.managers.user', User=mocked_user):
+        users = UserManager('~/path/to/admin/gitolite/repo')
+
+        eq_('test_username', users.create('test_username', 'key_path'))
+        mocked_user.assert_called_once_with(mocked_path, mocked_git,
+                                            'test_username', keys=['key_path'])
