d5ed8f572e43e5567fc5461242b0728a036bc46a
vtemian <vladtemian@gmail.com>
2014-05-19 14:54:08 +0300

    Test: if get method is called, it should proxy it to User.get_by_name and return a new User instance

--- tests/test_user.py
+++ tests/test_user.py
@@ -29,3 +29,19 @@ class TestUser(TestCase):
         eq_('test_username', users.create('test_username', 'key_path'))
         mocked_user.assert_called_once_with(mocked_path, mocked_git,
                                             'test_username', keys=['key_path'])
+
+  def test_get_user(self):
+    mocked_user = MagicMock(return_value='test_username')
+    mocked_git = MagicMock()
+    mocked_path = MagicMock()
+
+    with patch.multiple('pyolite.managers.manager',
+                        Git=MagicMock(return_value=mocked_git),
+                        Path=MagicMock(return_value=mocked_path)):
+      with patch.multiple('pyolite.managers.user', User=mocked_user):
+        users = UserManager('~/path/to/admin/gitolite/repo')
+        users.get('test_user')
+
+        mocked_user.get_by_name.assert_called_once_with('test_user',
+                                                        mocked_path,
+                                                        mocked_git)
