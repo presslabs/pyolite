d88c14eb106b75a94c0deefb462f1a67dbc12e80
vtemian <vladtemian@gmail.com>
2014-05-21 11:22:40 +0300

    Get users from a repom without groups

--- pyolite/models/lists/users.py
+++ pyolite/models/lists/users.py
@@ -1,7 +1,28 @@
+import re
+
+from unipath import Path
+
+
 class ListUsers(object):
   def __init__(self, repo):
     self.repo = repo
-    self._users = []
+    self._users = self._get_users()
+
+  def _get_users(self):
+    # TODO: check for groups
+    users = []
+    repo_config = Path(self.repo.path, 'conf/repos/',
+                       "%s.conf" % self.repo.name)
+
+    with open(str(repo_config)) as f:
+      config = f.read()
+      for match in re.compile('=( *)(\w+)').finditer(config):
+        users.append(match.group(2))
+
+    return users
+
+  def append(self, item):
+    pass
 
   def __iter__(self):
     for user in self._user:
@@ -17,5 +38,5 @@ class ListUsers(object):
     for item in items:
       self.append(item)
 
-  def append(self, item):
-    pass
+  def __str__(self):
+    return "['%s']" % ', '.join(self._users)
