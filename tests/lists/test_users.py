f3e61597da61729a5597770c71a4e9d41c224fac
vtemian <vladtemian@gmail.com>
2014-05-22 16:31:21 +0300

    Hackish way to play with pythons object

--- tests/lists/test_users.py
+++ tests/lists/test_users.py
@@ -4,6 +4,7 @@ from mock import MagicMock, patch, call
 from nose.tools import eq_
 
 from pyolite.models.lists.users import ListUsers
+from pyolite.models.user import User
 
 
 class TestUserList(TestCase):
@@ -26,7 +27,6 @@ class TestUserList(TestCase):
     with patch.multiple('pyolite.models.lists.users',
                         re=mocked_re, Path=mocked_path):
       repo_users = ListUsers(mocked_repository)
-
       eq_(repo_users._get_users(), ['user1', 'user2'])
 
       mocked_re.compile.has_calls([call('=( *)(\w+)')])
@@ -37,3 +37,26 @@ class TestUserList(TestCase):
 
       mocked_path.assert_called_once_with(mocked_path.path, 'conf/repos/',
                                           "users.conf")
+
+  def test_get_user_if_user_is_string(self):
+    mocked_git = MagicMock()
+    mocked_path = MagicMock()
+    mocked_repository = MagicMock()
+
+    class MockUser(User):
+      @classmethod
+      def get_by_name(cls, user, path, repo):
+        return my_user
+
+    my_user = MockUser(mocked_path, mocked_git, 'vlad',
+                       keys=['tests/fixtures/simple_key.pub'])
+
+    _get_users = ListUsers._get_users
+    ListUsers._get_users = lambda x: []
+    with patch.multiple('pyolite.models.lists.users',
+                        Path=mocked_path,
+                        User=MockUser):
+      repo_users = ListUsers(mocked_repository)
+
+      eq_(repo_users._get_user('user'), my_user)
+    ListUsers._get_users = _get_users
