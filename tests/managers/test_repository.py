10acd11b501891568b126b8eed778720bd8d77b0
vtemian <vladtemian@gmail.com>
2014-05-22 11:24:31 +0300

    If repository already exists, raise a ValueError exception

--- tests/managers/test_repository.py
+++ tests/managers/test_repository.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from mock import MagicMock, patch
-from nose.tools import eq_
+from nose.tools import eq_, raises
 
 from pyolite.managers.repository import RepositoryManager
 
@@ -26,3 +26,24 @@ class TestRepositoryManager(TestCase):
         mocked_repository.get_by_name.assert_called_once_with('my_repo',
                                                               mocked_path,
                                                               mocked_git)
+
+  @raises(ValueError)
+  def test_create_new_repository_that_already_exists(self):
+    mocked_path = MagicMock()
+    mocked_git = MagicMock()
+    mocked_repository = MagicMock()
+
+    mocked_file = MagicMock()
+    mocked_path = MagicMock()
+
+    mocked_path.return_value = mocked_file
+    mocked_file.exists.return_value = True
+
+    with patch.multiple('pyolite.managers.manager',
+                        Path=MagicMock(return_value=mocked_path),
+                        Git=MagicMock(return_value=mocked_git)):
+      with patch.multiple('pyolite.managers.repository',
+                          Path=mocked_path,
+                          Repository=mocked_repository):
+        repos = RepositoryManager('/path/to/admin/repo/')
+        repos.create('already_exists')
