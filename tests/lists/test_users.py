5da332b7b5780b2235999dcb598f19e2808e7a1c
vtemian <vladtemian@gmail.com>
2014-05-23 13:26:38 +0300

    If we add invalid permission, raise ValueError

--- tests/lists/test_users.py
+++ tests/lists/test_users.py
@@ -105,3 +105,28 @@ class TestUserList(Spec):
       except ValueError:
         ListUsers._get_users = _get_users
         ListUsers._get_user = _get_user
+
+  def test_if_we_add_invalid_permissions_it_should_raise_ValueError(self):
+    mocked_path = MagicMock()
+    mocked_re = MagicMock()
+    mocked_repository = MagicMock()
+    mocked_user = MagicMock()
+
+    mocked_user.name = 'another_user'
+
+    mocked_path.return_value = 'tests/fixtures/users.conf'
+    mocked_re.compile('=( *)(\w+)').findall.return_value = [(None, 'user')]
+
+    _get_users = ListUsers._get_users
+    _get_user = ListUsers._get_user
+    ListUsers._get_users = lambda x: []
+    ListUsers._get_user = lambda x, user: mocked_user
+    with patch.multiple('pyolite.models.lists.users',
+                        Path=mocked_path, re=mocked_re):
+      repo_users = ListUsers(mocked_repository)
+
+      try:
+        repo_users.add('test', 'hiRW+')
+      except ValueError:
+        ListUsers._get_users = _get_users
+        ListUsers._get_user = _get_user
