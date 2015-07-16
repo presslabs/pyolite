3a7ec253470f44c815ec9cf628a312ef500b988d
vtemian <vladtemian@gmail.com>
2015-07-16 17:41:47 +0300

    Add users key after you create it

--- pyolite/managers/user.py
+++ pyolite/managers/user.py
@@ -11,7 +11,9 @@ class UserManager(Manager):
     if key is None and key_path is None:
       raise ValueError('You need to specify a key or key_path')
 
-    return User(self.path, self.git, name, keys=[key or key_path])
+    user = User(self.path, self.git, name)
+    user.keys.append(key or key_path)
+    return user
 
   def get(self, name):
     return User.get_by_name(name, self.path, self.git)
