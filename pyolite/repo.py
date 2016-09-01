11aa659791ba5fc5c297578c0156759f152c24d3
Bogdan Petrea <bogdan@presslabs.com>
2016-09-01 20:22:20 +0300

    Added filelocking for Repo operations.

--- pyolite/repo.py
+++ pyolite/repo.py
@@ -1,3 +1,4 @@
+import fcntl
 import re
 
 
@@ -7,11 +8,16 @@ class Repo(object):
 
     def replace(self, pattern, string):
         with open(str(self.path), 'r+') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
+
             content = f.read()
             content = re.sub(pattern, string, content)
+
             f.seek(0)
             f.write(content)
             f.truncate()
+
+            fcntl.flock(f, fcntl.LOCK_UN)
 
     @property
     def users(self):
@@ -20,7 +26,9 @@ class Repo(object):
 
         users = []
         with open(str(self.path)) as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
             config = f.read()
+            fcntl.flock(f, fcntl.LOCK_UN)
             for match in re.compile('=( *)(\w+)').finditer(config):
                 users.append(match.group(2))
 
@@ -28,4 +36,6 @@ class Repo(object):
 
     def write(self, string):
         with open(self.path, 'a') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
             f.write(string)
+            fcntl.flock(f, fcntl.LOCK_UN)
